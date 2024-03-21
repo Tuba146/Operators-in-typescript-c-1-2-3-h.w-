8e85fc42e01eb3039b11e2922c8eeae8ec29fbca
[Tuba Asif] <[tubaasif146@gmail.com]>
2024-03-21 14:08:41 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+//Opertos in typescript (h.w) , (c= 1,2,3)
+//Addition
+var num1 = 2;
+var num2 = 3;
+var cart = num1 + num2;
+console.log(cart); //5
+//Subtraction
+var num3 = 5;
+var num4 = 2;
+var total = num3 - num4;
+console.log(total); //3
+//Multiplication
+var bags = 5;
+var appels = 4;
+var totalthings = bags * appels;
+console.log(totalthings); //20
+//Division
+var tabel = 20;
+var purse = 4;
+var allthings = tabel / purse;
+console.log(allthings); //5
+//Exponentiation
+var oranges = 5;
+var mango = 5;
+var power = Math.pow(oranges, 2);
+console.log(power); // 25
+//Modulus
+var ring = 5;
+var locket = 2;
+var reminder = ring % locket;
+console.log(reminder); //1
+//Unary operators
+var a = 5;
+console.log(++a); //6
+var b = 2;
+console.log(--b); //1
+var c = 6;
+var d = 3;
+var e;
+e = ++c + c++ + --d + d-- + c + d++ + d;
+console.log(e); // 21
---
+++ main.ts
@@ -0,0 +1,58 @@
+//Opertos in typescript (h.w) , (c= 1,2,3)
+
+
+//Addition
+let num1: number = 2;
+let num2: number = 3;
+let cart: number = num1 + num2;
+console.log(cart) //5
+
+//Subtraction
+let num3: number = 5;
+let num4: number = 2;
+let total: number = num3 - num4
+console.log(total) //3
+
+//Multiplication
+let bags: number = 5
+let appels: number = 4
+let totalthings: number = bags * appels
+console.log(totalthings)//20
+
+//Division
+let tabel: number = 20;
+let purse: number = 4;
+let allthings: number = tabel / purse
+console.log(allthings) //5
+
+//Exponentiation
+let oranges: number = 5
+let mango: number = 5
+let power: number = oranges ** 2
+console.log(power) // 25
+
+//Modulus
+let ring = 5
+let locket = 2
+let reminder = ring % locket
+console.log(reminder) //1
+
+//Unary operators
+let a=5
+console.log(++a)//6
+
+let b=2
+console.log(--b)//1
+
+let c: number = 6
+let d: number = 3
+let e: number ;
+e= ++c + c++ + --d + d-- + c + d++ + d;
+console.log(e) // 29
+
+
+
+
+
+
+
